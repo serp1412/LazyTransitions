5e6582259eb9be4d773cd34199fe43283aee9f78
Serghei Catraniuc <serghei.catraniuc@yopeso.com>
2017-01-12 14:17:08 +0200

    Comment out finger tips for now

--- LazyTransitionsExample/LazyTransitionsExample/AppDelegate.swift
+++ LazyTransitionsExample/LazyTransitionsExample/AppDelegate.swift
@@ -15,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        if let fingerWindow = window as? MBFingerTipWindow {
-            fingerWindow.alwaysShowTouches = true
-        }
+//        if let fingerWindow = window as? MBFingerTipWindow {
+//            fingerWindow.alwaysShowTouches = true
+//        }
         // Override point for customization after application launch.
         return true
     }
